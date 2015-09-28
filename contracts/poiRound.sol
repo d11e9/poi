6362cc6e4f450fca1e5f908b5f4f3b952d12c540
Doug A. <d11e9@users.noreply.github.com>
2015-09-28 14:43:21 +0100

    Update poiRound.sol

--- contracts/poiRound.sol
+++ contracts/poiRound.sol
@@ -10,15 +10,18 @@ contract POIRound {
     struct User {
         uint groupNumber;
         uint validations;
+        bool registered;
     }
     
     mapping (uint => Group) public groups;
     mapping (address => User) public users;
     
     uint public numGroups;
+    uint public numRegisteredUsers;
     uint public genesisBlock;
     uint public roundLengthInBlocks;
     uint public registrationTimeInBlocks;
+    uint public assignmentWindowInBlocks;
     uint public validationWindowInBlocks;
     
     function POIRound() {
@@ -26,12 +29,24 @@ contract POIRound {
         genesisBlock = block.number;
         roundLengthInBlocks = 30 * 1000 * 1000;
         registrationTimeInBlocks = 10 * 1000;
+        assignmentWindowInBlocks = 5 * 1000;
         validationWindowInBlocks = 3 * 1000;
     }
     
     function register () returns(uint groupNum){
         if ((block.number < genesisBlock + registrationTimeInBlocks)
+        && (!users[msg.sender].registered)) {
+            users[msg.sender].registered = true;
+            numRegisteredUsers++;
+        }
+    }
+    
+    function assignGroup () returns(uint groupNum) {
+        if ((block.number > genesisBlock + registrationTimeInBlocks)
+        && (block.number < genesisBlock + registrationTimeInBlocks + assignmentWindowInBlocks)
+        && (users[msg.sender].registered)
         && (users[msg.sender].groupNumber == 0)) {
+            // TODO: now that we have an assignment window, dont assign in order
             if (groups[numGroups].numUsers == 5) { numGroups++; }
             users[msg.sender].groupNumber = numGroups;
             groups[numGroups].numUsers++;
@@ -40,10 +55,10 @@ contract POIRound {
     }
     
     function verify () returns(bool success){
-        if ((block.number > genesisBlock + registrationTimeInBlocks) 
-        && (block.number < genesisBlock + registrationTimeInBlocks + validationWindowInBlocks) 
+        if ((block.number > genesisBlock + registrationTimeInBlocks + assignmentWindowInBlocks) 
+        && (block.number < genesisBlock + registrationTimeInBlocks + assignmentWindowInBlocks + validationWindowInBlocks) 
         && (users[msg.sender].groupNumber != 0)) {
-            
+            // TODO: verify
         }
     }
 }
